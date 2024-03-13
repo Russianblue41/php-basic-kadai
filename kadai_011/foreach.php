eed07bef25f559ed5ef985a60a535017c09010d7
Russianblue41 <158019791+Russianblue41@users.noreply.github.com>
2024-03-14 05:18:12 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charaset="UTF-8">
+  <title>PHP11章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetable = [
+      '名前' => '玉ねぎ',
+      '値段' => 200,
+      '産地' => '北海道'
+    ];
+
+    foreach ($vegetable as $key => $value) {
+      echo "{$key}:{$value}<br>";
+    }
+    ?>
+  </p>
+</body>
+
+</html>
