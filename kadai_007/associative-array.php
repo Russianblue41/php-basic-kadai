f2226ac7f079eb029a60bcc7fb207dae9dd963dc
Russianblue41 <158019791+Russianblue41@users.noreply.github.com>
2024-03-14 01:12:23 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charaset="UTF-8">
+  <title>PHP7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($vegetables);
+    ?>
+  </p>
+</body>
+
+</html>
